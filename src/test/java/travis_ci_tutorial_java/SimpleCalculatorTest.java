3a8a5704a9e567be2a855e3d0827f6bacbf341d9
shanerphillips1 <shaneryanphillips@hotmail.com>
2025-12-09 00:22:11 -0500

    Refactor SimpleCalculatorTest into SimpleCalculator

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -1,38 +1,23 @@
 package travis_ci_tutorial_java;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
+public class SimpleCalculator {
 
-public class SimpleCalculatorTest {
-
-    @Test
-    public void testAdd() {
-        SimpleCalculator calc = new SimpleCalculator();
-        assertEquals(2, calc.add(1, 1));
+    public int add(int a, int b) {
+        return a + b;
     }
 
-    @Test
-    public void testMinus() {
-        SimpleCalculator calc = new SimpleCalculator();
-        assertEquals(2, calc.minus(5, 3));
+    public int minus(int a, int b) {
+        return a - b;
     }
 
-    @Test
-    public void testMultiply() {
-        SimpleCalculator calc = new SimpleCalculator();
-        assertEquals(6, calc.multiply(2, 3));
+    public int multiply(int a, int b) {
+        return a * b;
     }
 
-    @Test
-    public void testDivide() {
-        SimpleCalculator calc = new SimpleCalculator();
-        assertEquals(2, calc.divide(6, 3));
-    }
-
-    // ✅ STEP 34: edge-case test (divide by zero)
-    @Test(expected = ArithmeticException.class)
-    public void testDivideByZero() {
-        SimpleCalculator calc = new SimpleCalculator();
-        calc.divide(5, 0);
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
     }
 }
