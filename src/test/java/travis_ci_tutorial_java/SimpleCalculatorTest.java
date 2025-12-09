defb609e101e916e66036623047ccc5126b85020
shanerphillips1 <shaneryanphillips@hotmail.com>
2025-12-08 23:31:36 -0500

    Fix testAdd method and add testMinus method

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -1,14 +1,19 @@
 package travis_ci_tutorial_java;
 
-import java.util.*;
-
 import static org.junit.Assert.*;
-import org.junit.*;
+import org.junit.Test;
 
 public class SimpleCalculatorTest {
-	@Test
-	public void testAdd() {
-		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(1, 1), 0);
-	}
+
+    @Test
+    public void testAdd() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(2, calc.add(1, 1));
+    }
+
+    @Test
+    public void testMinus() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(2, calc.minus(5, 3));
+    }
 }
